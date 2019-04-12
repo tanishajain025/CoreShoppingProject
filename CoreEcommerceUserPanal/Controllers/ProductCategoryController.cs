0c4ec9eef1777557960c9cc4dfba298dd5ba02c6
tanishajain025 <tanishajain059@gmail.com>
2019-04-12 16:52:14 +0530

    updated using testing

--- CoreEcommerceUserPanal/Controllers/ProductCategoryController.cs
+++ CoreEcommerceUserPanal/Controllers/ProductCategoryController.cs
@@ -9,17 +9,39 @@ namespace CoreEcommerceUserPanal.Controllers
 {
     public class ProductCategoryController : Controller
     {
-        ShoppingProjectContext context = new ShoppingProjectContext();
+        //ShoppingProjectContext context = new ShoppingProjectContext();
+        private readonly ShoppingProjectContext _context;
+
+        public ProductCategoryController(ShoppingProjectContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
 
-            var pc = context.Categories.ToList();
+            var pc = _context.Categories.ToList();
             return View(pc);
         }
-        public IActionResult ProductDisplay(int id)
+        [HttpGet("{id}")]
+        public async Task<IActionResult> ProductDisplay(int? id)
         {
-            var p = context.Products.Where(x => x.ProductCategoryId == id).ToList();
+            var p = _context.Products.Where(x => x.ProductCategoryId == id).ToList();
             return View(p);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            var Categories = await _context.Categories.FindAsync(id);
+            if (Categories == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Categories);
         }
     }
 }
--- EcommerceUserPanal.Tests/ProductCategoryTestController.cs
+++ EcommerceUserPanal.Tests/ProductCategoryTestController.cs
@@ -1,10 +1,64 @@
-﻿using System;
+﻿using CoreEcommerceUserPanal.Controllers;
+using CoreEcommerceUserPanal.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace EcommerceUserPanal.Tests
 {
-    class ProductCategoryTestController
+   public class ProductCategoryTestController
     {
+        private ShoppingProjectContext _context;
+
+        public static DbContextOptions<ShoppingProjectContext>
+            dbContextOptions
+        { get; set; }
+
+        public static string connectionString =
+ "Data Source=TRD-519; Initial Catalog=ShoppingProject;Integrated Security=true;";
+
+        static ProductCategoryTestController()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<ShoppingProjectContext>()
+                .UseSqlServer(connectionString).Options;
+        }
+        public ProductCategoryTestController()
+        {
+            _context = new ShoppingProjectContext(dbContextOptions);
+        }
+        [Fact]
+        public async void Task_GetPcById_Return_OkResult()
+        {
+            var controller = new ProductCategoryController(_context);
+            var PcId = 19;
+            var data = await controller.ProductDisplay(PcId);
+            Assert.IsType<ViewResult>(data);
+        }
+        [Fact]
+        public async void Task_GetpcById_Return_FailResult()
+        {
+            var controller = new ProductCategoryController(_context);
+            var PcId = 25;
+            var data = await controller.Get(PcId);
+            Assert.IsType<NotFoundResult>(data);
+        }
+        [Fact]
+        public async void Task_GetUserById_MatchResult()
+        {
+            var controller = new ProductCategoryController(_context);
+            int id = 1;
+            var data = await controller.Get(id);
+            Assert.IsType<OkObjectResult>(data);
+            var OkResult = data.Should().BeOfType<OkObjectResult>().Subject;
+            var user = OkResult.Value.Should().BeAssignableTo<Categories>().Subject;
+
+            Assert.Equal("Top wear", user.CategoryName);
+            Assert.Equal("Comfortable wear in every season", user.CategoryDescription);
+
+        }
     }
 }
